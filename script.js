807836d481386c45e664dc65c48417a69a7300c7
mrbun1906 <81362489+mrbun1906@users.noreply.github.com>
2021-05-07 22:24:27 +0700

    1st Test Ozone web

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ $(".menu-hamburger").click( function(){
 $(".menu-contact").click(function() {
     $(".contact-name").slideToggle('slow');
 });
+$('.wrapper-slider-customers').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+$('.brands-mobile').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  $(".footer-contact-item-1").click( function(){
+    $(".footer-contact-item-1").toggleClass("active");
+    $(".footer-contact-item-2").slideToggle('slow');
+});
