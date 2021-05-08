2404e01010d0ee48b45548141fe1034dbe9d2513
mrbun1906 <81362489+mrbun1906@users.noreply.github.com>
2021-05-08 13:12:38 +0700

    2nd Test Ozone Web

--- script.js
+++ script.js
@@ -8,46 +8,46 @@
 //         containerMenu.classList.add("active");
 //     }    
 // }
-$(".menu-hamburger").click( function(){
-    $(".menu-hamburger").toggleClass("active");
-    $(".menu-banner-mobile").slideToggle('slow');
+$(".menu-hamburger").click(function () {
+  $(".menu-hamburger").toggleClass("active");
+  $(".menu-banner-mobile").slideToggle('slow');
 });
-$(".menu-contact").click(function() {
-    $(".contact-name").slideToggle('slow');
+$(".menu-contact").click(function () {
+  $(".contact-name").slideToggle('slow');
 });
 $('.wrapper-slider-customers').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+  dots: true,
+  infinite: true,
+  autoRun: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
 });
 $('.brands-mobile').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
-    ]
-  });
-  $(".footer-contact-item-1").click( function(){
-    $(".footer-contact-item-1").toggleClass("active");
-    $(".footer-contact-item-2").slideToggle('slow');
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+$(".footer-contact-item-1").click(function () {
+  $(".footer-contact-item-1").toggleClass("active");
+  $(".footer-contact-item-2").slideToggle('slow');
 });
